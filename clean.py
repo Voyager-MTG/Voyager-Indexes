0ff4e77337e67b55a798e6d90d0fa10d73833ff2
Aanginer <decodedaygaming@gmail.com>
2026-03-01 10:24:12 -0800

    2026-03-01_10:24:11

---
+++ clean.py
@@ -0,0 +1,13 @@
+# import os, shutil, sys
+
+# set_code = sys.argv[1]
+
+# try:
+#     shutil.rmtree('OUT.mse-set')
+# except:
+#     print('Unable to remove OUT')
+
+# shutil.copytree(set_code + '.mse-set', 'OUT.mse-set')
+
+# for card in os.listdir('OUT.mse-set'):
+#     if 
